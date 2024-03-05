9c692f1f0fcd6c4685f2b2b5068e903240644e06
Dennis Torres <dtorres@spear.ai>
2024-03-05 13:38:33 -0500

    Add `hasLabel` control to Switch story

--- packages/ui/src/components/switch/switch.stories.tsx
+++ packages/ui/src/components/switch/switch.stories.tsx
@@ -3,12 +3,13 @@ import { Form, Switch } from "react-aria-components";
 import { useIntl } from "react-intl";
 
 const PreviewSwitch = (properties: {
+  hasLabel: boolean;
   isDisabled: boolean;
   isPrimary: boolean;
   isReadonly: boolean;
   isSquished: boolean;
 }) => {
-  const { isDisabled, isPrimary, isReadonly, isSquished } = properties;
+  const { hasLabel, isDisabled, isPrimary, isReadonly, isSquished } = properties;
   const intl = useIntl();
 
   return (
@@ -26,12 +27,14 @@ const PreviewSwitch = (properties: {
             >
               <span className="pointer-events-none relative inline-block size-5 rounded-full bg-white shadow transition-all duration-200 ease-in-out will-change-transform translate-x-0 group-selected:translate-x-5 group-disabled:bg-neutral-2 group-disabled:shadow-none" />
             </span>
-            <span className="ms-3 flex-1 text-sm font-medium leading-6 text-neutral-12 group-disabled:text-neutral-11">
-              {intl.formatMessage({
-                defaultMessage: "Enabled",
-                id: "V52jNn",
-              })}
-            </span>
+            {hasLabel ? (
+              <span className="ms-3 flex-1 text-sm font-medium leading-6 text-neutral-12 group-disabled:text-neutral-11">
+                {intl.formatMessage({
+                  defaultMessage: "Enabled",
+                  id: "V52jNn",
+                })}
+              </span>
+            ) : null}
           </Switch>
         </div>
       </Form>
@@ -47,6 +50,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Standard: Story = {
   args: {
+    hasLabel: true,
     isDisabled: false,
     isPrimary: false,
     isReadonly: false,
