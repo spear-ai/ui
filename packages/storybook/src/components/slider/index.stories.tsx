d22c259b8ad4d815e7e05ea43104b9b0bf5af9fc
Dennis Torres <dtorres@spear.ai>
2024-08-14 16:19:45 -0400

    WIP

--- packages/storybook/src/components/slider/index.stories.tsx
+++ packages/storybook/src/components/slider/index.stories.tsx
@@ -1,11 +1,23 @@
-import { Slider, SliderDescription, SliderLabel } from "@spear-ai/ui/components/slider";
+import {
+  Slider,
+  SliderDescription,
+  SliderLabel,
+  SliderLabels,
+  SliderOutput,
+  SliderRange,
+  SliderThumb,
+  SliderTrack,
+} from "@spear-ai/ui/components/slider";
 import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 import { Form } from "react-aria-components";
 import { useIntl } from "react-intl";
 
 const PreviewSlider = (properties: {
+  color: "neutral" | "primary";
   hasLabel: boolean;
   hasLabelDescription: boolean;
+  hasRange: boolean;
   hasValence: boolean;
   isDisabled: boolean;
   isSquished: boolean;
@@ -17,8 +29,10 @@ const PreviewSlider = (properties: {
   variant: "soft" | "surface";
 }) => {
   const {
+    color,
     hasLabel,
     hasLabelDescription,
+    hasRange,
     hasValence,
     isDisabled,
     isSquished,
@@ -30,36 +44,50 @@ const PreviewSlider = (properties: {
     variant,
   } = properties;
   const intl = useIntl();
+  const [value, setValue] = useState(
+    Math.max(Math.min(Math.round(maximumValue / 4), maximumValue), minimumValue),
+  );
 
   return (
     <div className={`w-full ${isSquished ? "max-w-36" : "max-w-xs"}`}>
       <Form className="relative w-full">
         <Slider
           className="w-full"
+          color={color}
+          formatOptions={{}}
           hasValence={hasValence}
           isDisabled={isDisabled}
           maxValue={maximumValue}
           minValue={minimumValue}
+          onChange={setValue}
           orientation={orientation}
           originValue={originValue}
+          value={Math.max(Math.min(value, maximumValue), minimumValue)}
           variant={variant}
         >
-          {hasLabel ? (
-            <SliderLabel>
-              {intl.formatMessage({
-                defaultMessage: "Volume",
-                id: "y867Vs",
-              })}
-            </SliderLabel>
-          ) : null}
-          {hasLabel && hasLabelDescription ? (
-            <SliderDescription>
-              {intl.formatMessage({
-                defaultMessage: "How loud until the neighbors complain.",
-                id: "bEWLDO",
-              })}
-            </SliderDescription>
-          ) : null}
+          <SliderLabels>
+            {hasLabel ? (
+              <SliderLabel>
+                {intl.formatMessage({
+                  defaultMessage: "Volume",
+                  id: "y867Vs",
+                })}
+              </SliderLabel>
+            ) : null}
+            {hasLabel && hasLabelDescription ? (
+              <SliderDescription>
+                {intl.formatMessage({
+                  defaultMessage: "How loud until the neighbors complain.",
+                  id: "bEWLDO",
+                })}
+              </SliderDescription>
+            ) : null}
+            <SliderOutput>{({ state }) => `${state.getThumbValue(0)} dB`}</SliderOutput>
+          </SliderLabels>
+          <SliderTrack className="h-56">
+            {hasRange ? <SliderRange /> : null}
+            <SliderThumb />
+          </SliderTrack>
         </Slider>
       </Form>
     </div>
@@ -74,8 +102,10 @@ type Story = StoryObj<typeof meta>;
 
 export const Standard: Story = {
   args: {
+    color: "neutral",
     hasLabel: true,
     hasLabelDescription: true,
+    hasRange: false,
     hasValence: false,
     isDisabled: false,
     isSquished: false,
@@ -87,6 +117,12 @@ export const Standard: Story = {
     variant: "surface",
   },
   argTypes: {
+    color: {
+      control: {
+        type: "select",
+      },
+      options: ["neutral", "primary"],
+    },
     orientation: {
       control: { type: "select" },
       options: ["horizontal", "vertical"],
--- packages/ui/src/components/slider.tsx
+++ packages/ui/src/components/slider.tsx
@@ -1,5 +1,12 @@
-import { ComponentPropsWithoutRef, ElementRef, forwardRef, HTMLAttributes } from "react";
-import { Label as LabelPrimitive, Slider as SliderPrimitive } from "react-aria-components";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, HTMLAttributes, useContext } from "react";
+import {
+  Label as LabelPrimitive,
+  Slider as SliderPrimitive,
+  SliderOutput as SliderOutputPrimitive,
+  SliderStateContext,
+  SliderThumb as SliderThumbPrimitive,
+  SliderTrack as SliderTrackPrimitive,
+} from "react-aria-components";
 import { cx } from "@/helpers/cx";
 
 export const Slider = forwardRef<
@@ -31,6 +38,15 @@ export const Slider = forwardRef<
 
 Slider.displayName = "Slider";
 
+export const SliderLabels = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...properties }, reference) => {
+    const mergedClassName = cx("flex flex-wrap items-end justify-between", className);
+    return <div className={mergedClassName} {...properties} ref={reference} />;
+  },
+);
+
+SliderLabels.displayName = "SliderLabels";
+
 export const SliderLabel = forwardRef<
   ElementRef<typeof LabelPrimitive>,
   ComponentPropsWithoutRef<typeof LabelPrimitive>
@@ -47,7 +63,7 @@ SliderLabel.displayName = "SliderLabel";
 export const SliderDescription = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(
   ({ className, ...properties }, reference) => {
     const mergedClassName = cx(
-      "text-neutral-11 group-disabled:text-neutral-9 -mt-1 mb-2 text-base/6 sm:text-sm/6",
+      "text-neutral-11 group-disabled:text-neutral-9 -mt-1 mb-2 w-full text-base/6 sm:text-sm/6",
       className,
     );
     return <p className={mergedClassName} {...properties} ref={reference} />;
@@ -55,3 +71,59 @@ export const SliderDescription = forwardRef<HTMLParagraphElement, HTMLAttributes
 );
 
 SliderDescription.displayName = "SliderDescription";
+
+export const SliderOutput = forwardRef<
+  ElementRef<typeof SliderOutputPrimitive>,
+  ComponentPropsWithoutRef<typeof SliderOutputPrimitive> & { className?: string | undefined }
+>(({ className, ...properties }, reference) => {
+  const mergedClassName = cx(
+    "text-neutral-11 mb-0.5 ms-auto text-base/6 text-sm tabular-nums sm:text-sm/6",
+    className,
+  );
+  return <SliderOutputPrimitive className={mergedClassName} {...properties} ref={reference} />;
+});
+
+SliderOutput.displayName = "SliderOutput";
+
+export const SliderTrack = forwardRef<
+  ElementRef<typeof SliderTrackPrimitive>,
+  ComponentPropsWithoutRef<typeof SliderTrackPrimitive> & { className?: string | undefined }
+>(({ className, ...properties }, reference) => {
+  const mergedClassName = cx(
+    "bg-neutral-a-3 outline-neutral-a-7 relative rounded-full -outline-offset-1 group-data-[orientation=horizontal]:h-2 group-data-[orientation=horizontal]:w-full group-data-[orientation=vertical]:w-2 group-data-[variant=surface]:outline",
+    className,
+  );
+  return <SliderTrackPrimitive className={mergedClassName} {...properties} ref={reference} />;
+});
+
+SliderTrack.displayName = "SliderTrack";
+
+export const SliderRange = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...properties }, reference) => {
+    const state = useContext(SliderStateContext);
+    const fill = `${state.getThumbPercent(0) * 100}%`;
+    const width = state.orientation === "horizontal" ? fill : "100%";
+    const height = state.orientation === "vertical" ? fill : "100%";
+    const mergedClassName = cx(
+      "outline-neutral-a-7 group-data-[color=primary]:outline-primary-a-7 bg-neutral-9 group-data-[color=primary]:bg-primary-9 absolute bottom-0 start-0 rounded-full -outline-offset-1 group-data-[variant=surface]:outline",
+      className,
+    );
+    // eslint-disable-next-line react/forbid-dom-props
+    return <div className={mergedClassName} {...properties} ref={reference} style={{ height, width }} />;
+  },
+);
+
+SliderTrack.displayName = "SliderRange";
+
+export const SliderThumb = forwardRef<
+  ElementRef<typeof SliderThumbPrimitive>,
+  ComponentPropsWithoutRef<typeof SliderThumbPrimitive> & { className?: string | undefined }
+>(({ className, ...properties }, reference) => {
+  const mergedClassName = cx(
+    "shadow-neutral-a-9 bg-neutral-9 absolute h-4 w-4 rounded-full bg-white shadow group-data-[orientation=horizontal]:top-1 group-data-[orientation=vertical]:start-1 dark:shadow-none",
+    className,
+  );
+  return <SliderThumbPrimitive className={mergedClassName} {...properties} ref={reference} />;
+});
+
+SliderThumb.displayName = "SliderThumb";
